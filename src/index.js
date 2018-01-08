51098054fb1b7580847f53dd1e00688d350bfea3
Christofer Johnson <christofer.d.johnson@gmail.com>
2018-01-08 16:59:29 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 
 import Product from './components/Product';
 
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.string.isRequired,
+}
+
+Product.defaultProps{
+  hasWatermark: false,
+
+}
+
+
 ReactDOM.render(
   <Product
     name="Dunder Mifflin"
